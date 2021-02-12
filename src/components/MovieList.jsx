27704345bfbd328f2d3731c8022ddc873f5af750
Giovani Lopes Diccini <giovani-diccini@pop-os.localdomain>
2021-02-12 16:27:02 -0300

    comitao bomba

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return <div>{this.props.movies.map((element, index) => <MovieCard key={`Movie Title ${index + 1}`} movie={element}/>)}</div>
+  }
+}
+
+export default MovieList;
